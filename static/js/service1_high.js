5ced9687f00d67b97eb984779ea27647c9faf9d8
YoonHwe <dbsghl676@gmail.com>
2021-08-08 17:04:53 +0900

    localstorage 기능 수정

--- static/js/service1_high.js
+++ static/js/service1_high.js
@@ -1,5 +1,9 @@
+const HIGH_TEST_RESULT_KEY = `high_test_result_${userName}`;
 const highResult = document.querySelector("#high_result");
 const savedHighResult = localStorage.getItem(HIGH_TEST_RESULT_KEY);
+if(savedHighResult === null){
+  savedHighResult = "?";
+}
 highResult.innerText = `최근 테스트 결과: ${savedHighResult}`;
 
 
@@ -12,7 +16,8 @@ function getHighCheckboxValue(event){//선택한 최고음정 가져오기
   }
   localStorage.setItem(HIGH_TEST_RESULT_KEY, result2);
   const newSavedHighResult = localStorage.getItem(HIGH_TEST_RESULT_KEY);
-  console.log(newSavedHighResult);
-  console.log(highResult);
+  if(newSavedHighResult === null){
+    newSavedHighResult = "?";
+  }
   highResult.innerText = `최근 테스트 결과: ${newSavedHighResult}`;
 }
